1a23c18f8b512fa49630abcf09980e8328d42d35
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-04-30 15:43:58 +0100

    yellow cell fixed

--- server.rb
+++ server.rb
@@ -7,6 +7,7 @@ require 'rack-flash'
 use Rack::Flash
 
 enable :sessions
+set :session_secret, '*&(^B234'
 
 def random_sudoku
     # we're using 9 numbers, 1 to 9, and 72 zeros as an input
@@ -28,28 +29,6 @@ def puzzle(sudoku)
     end
     new_sud  
 end
-
-  get '/solution' do
-    @current_solution = session[:solution]
-    # puts session[:solution]
-    erb :index
-  end
-
-  get '/' do
-    prepare_to_check_solution
-    generate_new_puzzle_if_necessary
-    @current_solution = session[:current_solution] || session[:puzzle]
-    @solution = session[:solution]
-    @puzzle = session[:puzzle]  
-    erb :index
-  end
-
-  post '/' do
-    cells = box_order_to_row_order(params["cell"])
-    session[:current_solution] = cells.map{ |value| value.to_i  }.join
-    session[:check_solution] = true
-    redirect to("/")
-  end
 
 def box_order_to_row_order(cells)
   boxes = cells.each_slice(9).to_a
@@ -81,14 +60,36 @@ end
     session[:check_solution] = nil
   end
 
+  get '/solution' do
+    @current_solution = session[:solution]
+    # puts session[:solution]
+    erb :index
+  end
+
+  get '/' do
+    prepare_to_check_solution
+    generate_new_puzzle_if_necessary
+    @current_solution = session[:current_solution] || session[:puzzle]
+    @solution = session[:solution]
+    @puzzle = session[:puzzle]  
+    erb :index
+  end
+
+  post '/' do
+    cells = box_order_to_row_order(params["cell"])
+    session[:current_solution] = cells.map{ |value| value.to_i  }.join
+    session[:check_solution] = true
+    redirect to("/")
+  end
+
   helpers do
 
   def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value) 
     must_be_guessed = puzzle_value == 0
-    tried_to_guess = current_solution_value.to_i != 0
+    tried_to_guess = (current_solution_value.to_i != 0)
     guessed_incorrectly = current_solution_value != solution_value
+
       if solution_to_check &&
-           must_be_guessed &&
            tried_to_guess &&
            guessed_incorrectly
            'incorrect'
@@ -96,8 +97,7 @@ end
         'value-provided'
       end
     end
-  
-  
+   
   def cell_value(value)
     value.to_i == 0 ? '' : value
   end
